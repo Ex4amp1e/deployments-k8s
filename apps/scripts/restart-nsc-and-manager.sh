214e7f34a74b34b586b879ef83f875e4f2d98a49
Vladislav Byrgazov <vladislav.byrgazov@xored.com>
2024-11-13 14:24:06 +0500

    [tmp] test files
    Signed-off-by: Vladislav Byrgazov <vladislav.byrgazov@xored.com>

---
+++ apps/scripts/restart-nsc-and-manager.sh
@@ -0,0 +1,1 @@
+kubectl exec -n ns-ipam-policies first-nse -c nse -- kill 1 && kubectl exec -n nsm-system nsmgr-5pwwp -c nsmgr -- kill 1
---
+++ apps/scripts/restart-nse-and-registry.sh
@@ -0,0 +1,1 @@
+kubectl exec -n ns-ipam-policies first-nse -c nse -- kill 1 && kubectl exec -n nsm-system registry-k8s-5449575b77-69nfs -- kill 1
